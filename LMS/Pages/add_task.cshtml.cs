8e3b4e8541849ff1273eb7b0eed4af6f066dcdea
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2024-04-27 21:48:48 +0300

    to do list done feedback done

---
+++ LMS/Pages/add_task.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages
+{
+    public class add_taskModel : PageModel
+    {
+        [BindProperty]
+        public string new_task { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public List<string> AddTasks { get; set; }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            AddTasks.Add(new_task);
+            return (RedirectToPage("/studenthome", new { tasks = AddTasks }));
+
+        }
+    }
+}
---
+++ LMS/Pages/studenthome.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages
+{
+    public class studenthomeModel : PageModel
+    {
+        [BindProperty (SupportsGet =true)]
+        public string Studentname { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public List<string> tasks { get; set; }
+
+        public IActionResult OnPost(string id)
+        {
+            if (id == "Add Task")
+            {
+                return RedirectToPage("/add_task", new { AddTasks = tasks });
+            }
+            else
+            {
+                tasks.Remove(tasks[int.Parse(id)]);
+                List<string> result = tasks;
+                return RedirectToPage("/studenthome", new { tasks = result });
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
