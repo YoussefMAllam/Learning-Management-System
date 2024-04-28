db946c1356e8d2cca6030f954f5938ee0929c275
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2024-04-28 23:05:39 +0300

    Allam basic GUI done

---
+++ LMS/Pages/Teacher/coursehome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages.Teacher
+{
+    public class coursehomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
