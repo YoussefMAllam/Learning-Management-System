4ad68a4d044c4c31d07b1f8349034ca76a063339
amltarek <151668300+amltarek@users.noreply.github.com>
2024-05-02 19:56:54 +0300

    styleeee

--- LMS/Pages/Student/studenthome.cshtml.cs
+++ LMS/Pages/Student/studenthome.cshtml.cs
@@ -5,25 +5,9 @@ namespace LMS.Pages.Student
 {
     public class studenthomeModel : PageModel
     {
-        [BindProperty(SupportsGet = true)]
-        public string Studentname { get; set; }
+       
 
-        [BindProperty(SupportsGet = true)]
-        public List<string> tasks { get; set; }
-
-        public IActionResult OnPost(string id)
-        {
-            if (id == "Add Task")
-            {
-                return RedirectToPage("/add_task", new { AddTasks = tasks });
-            }
-            else
-            {
-                tasks.Remove(tasks[int.Parse(id)]);
-                List<string> result = tasks;
-                return RedirectToPage("/studenthome", new { tasks = result });
-            }
-        }
+       
 
         public void OnGet()
         {
