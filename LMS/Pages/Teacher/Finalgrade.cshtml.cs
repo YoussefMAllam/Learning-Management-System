2fca95c0f06431247addd32087eea034af70cd92
amltarek <151668300+amltarek@users.noreply.github.com>
2024-05-04 00:36:01 +0300

    aaa

---
+++ LMS/Pages/Teacher/Finalgrade.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages.Teacher
+{
+    public class FinalgradeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
