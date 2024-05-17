0ca252270ae7f961ba205fb3b125cf568d6b459d
DELL <DELL@DESKTOP-50DDNCA>
2024-05-17 19:43:12 +0300

    aa

--- LMS/Pages/Student/studenthome.cshtml.cs
+++ LMS/Pages/Student/studenthome.cshtml.cs
@@ -53,12 +53,12 @@ namespace LMS.Pages.Student
         public string CourseCode { get; set; }
         public string Semester { get; set; }
         public bool Done { get; set; }
-            id = HttpContext.Session.GetString("ID");
+           
         }
     }
 
 
-}
+
 
    
 
