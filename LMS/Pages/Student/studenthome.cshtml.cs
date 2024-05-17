64b3fb7197fbd467527d22b11049d23ad6f9d988
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2024-05-17 18:50:03 +0300

    Get id

--- LMS/Pages/Student/studenthome.cshtml.cs
+++ LMS/Pages/Student/studenthome.cshtml.cs
@@ -5,12 +5,11 @@ namespace LMS.Pages.Student
 {
     public class studenthomeModel : PageModel
     {
-       
-
-       
+        public string id { get; set; }
 
         public void OnGet()
         {
+            id= HttpContext.Session.GetString("ID");
 
         }
     }
