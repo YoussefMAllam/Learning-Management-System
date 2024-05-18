f0ac5d5d2f35a572b4c57dee633429c5b712a01d
YoussefMAllam <s-youssef.allam@zewailcity.edu.eg>
2024-05-19 00:25:04 +0300

    a3333333333

--- LMS/Pages/Teacher/teacherforum.cshtml.cs
+++ LMS/Pages/Teacher/teacherforum.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LMS.Models;
+using Microsoft.AspNetCore.Session;
+using System.Data;
+using Microsoft.AspNetCore.Http;
+using System.Globalization;
 
 namespace LMS.Pages.Teacher
 {
     public class teacherforumModel : PageModel
     {
+        private DB _db;
+        public DataTable dt=new DataTable();
+        public teacherforumModel()
+        {
+            _db = new DB();
+        }
         public void OnGet()
         {
+            dt=_db.getthreads();
+        }
+
+        public IActionResult OnPostView(string thread,string ccode)
+        {
+            return RedirectToPage("./ForumPage", new { title = thread, cocode = ccode });
         }
     }
 }
