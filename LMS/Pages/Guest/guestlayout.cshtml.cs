f4bb0559ef30fdadfb9b9844be7833df78d23f0e
DELL <DELL@DESKTOP-50DDNCA>
2024-05-20 00:32:31 +0300

    aaaa

---
+++ LMS/Pages/Guest/guestlayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Pages.Guest
+{
+    public class guestlayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
